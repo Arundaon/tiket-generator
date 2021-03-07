29307e296baa4453647486c10fa55e326f18df44
Arundaon Ramadhani Yudistira (TIF C) <ary@pop-os.localdomain>
2021-03-07 11:12:25 +0700

    remove password di respon find

--- app.js
+++ app.js
@@ -7,6 +7,20 @@ require("dotenv/config");
 const mongoose = require("mongoose");
 const Penumpang = require("./Models/Penumpang");
 const urlencodedParser = express.urlencoded({ extended: false });
+app.use(function (req, res, next) {
+    // Website you wish to allow to connect
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+    );
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "X-Requested-With,content-type"
+    );
+    res.setHeader("Access-Control-Allow-Credentials", true);
+    next();
+});
 app.set("view engine", "ejs");
 app.set("views", "website");
 app.get("/", async (req, res) => {
